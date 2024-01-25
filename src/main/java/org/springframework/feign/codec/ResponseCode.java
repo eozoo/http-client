76670298772afb56c16e4648e6b37af80afad524
jiangbo <bo.jiang@cowave.com>
2024-01-25 16:32:19 +0800

    增加202状态码，用于前端确认

--- src/main/java/org/springframework/feign/codec/ResponseCode.java
+++ src/main/java/org/springframework/feign/codec/ResponseCode.java
@@ -3,40 +3,46 @@ package org.springframework.feign.codec;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 
  * @author shanhuiming
- *
  */
 public enum ResponseCode {
-	
-	OK(HttpServletResponse.SC_OK, "Success"),
-	
-	BAD_REQUEST(HttpServletResponse.SC_BAD_REQUEST, "Bad Request"),
-	
-	UNAUTHORIZED(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"),
-	
-	FORBIDDEN(HttpServletResponse.SC_FORBIDDEN, "Forbidden"),
-	
-	TOKEN_INVALID_OR_EXPIRED(498, "Token changed or expired"),
 
-	INTERNAL_SERVER_ERROR(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "System Error"),
+    /**
+     * 处理成功
+     */
+    OK(HttpServletResponse.SC_OK, "Success"),
 
-	SYS_ERROR(597, "Sys Error");
+    /**
+     * 需要请求发起方确认是否继续处理的请求
+     */
+    ACCEPTED(HttpServletResponse.SC_ACCEPTED, "Accepted"),
 
-	private final int code;
-	
-	private final String desc;
-	
-	ResponseCode(int code, String desc) {
-		this.code = code;
-		this.desc = desc;
-	}
+    BAD_REQUEST(HttpServletResponse.SC_BAD_REQUEST, "Bad Request"),
 
-	public int getCode() {
-		return code;
-	}
+    UNAUTHORIZED(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"),
 
-	public String getDesc() {
-		return desc;
-	}
+    FORBIDDEN(HttpServletResponse.SC_FORBIDDEN, "Forbidden"),
+
+    TOKEN_INVALID_OR_EXPIRED(498, "Token changed or expired"),
+
+    INTERNAL_SERVER_ERROR(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "System Error"),
+
+    SYS_ERROR(597, "Sys Error");
+
+    private final int code;
+
+    private final String desc;
+
+    ResponseCode(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
