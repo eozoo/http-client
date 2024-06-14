440f6deb61f165786f2cf0a9453f0ddbd97e7ddf
shanhm1991 <shanhm1991@163.com>
2024-06-14 11:38:37 +0800

    code描述

--- src/main/java/org/springframework/feign/codec/ResponseCode.java
+++ src/main/java/org/springframework/feign/codec/ResponseCode.java
@@ -10,32 +10,32 @@ public enum ResponseCode {
     /**
      * 200
      */
-    OK(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase()),
+    OK(HttpStatus.OK.value(), "Success"),
 
     /**
      * 202
      */
-    ACCEPTED(HttpStatus.ACCEPTED.value(), HttpStatus.ACCEPTED.getReasonPhrase()),
+    ACCEPTED(HttpStatus.ACCEPTED.value(), "Accepted"),
 
     /**
      * 400
      */
-    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase()),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "Bad Request"),
 
     /**
      * 401
      */
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), "Unauthorized"),
 
     /**
      * 403
      */
-    FORBIDDEN(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase()),
+    FORBIDDEN(HttpStatus.FORBIDDEN.value(), "Forbidden"),
 
     /**
      * 429
      */
-    TOO_MANY_REQUESTS(HttpStatus.TOO_MANY_REQUESTS.value(), HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase()),
+    TOO_MANY_REQUESTS(HttpStatus.TOO_MANY_REQUESTS.value(), "Too Many Requests"),
 
     /**
      * 498
@@ -45,12 +45,12 @@ public enum ResponseCode {
     /**
      * 500
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error"),
 
     /**
      * 597
      */
-    SYS_ERROR(597, "Sys Error");
+    SYS_ERROR(597, "Business Error");
 
     private final int code;
 
